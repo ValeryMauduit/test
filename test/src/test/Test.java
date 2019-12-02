5feb3a85ce7cd66b2ae8905ee810cb4d2440c576
Valery Mauuduit <github@valerymauduit.fr>
2019-12-02 17:21:10 +0100

    Petite modif, méthode bonjour()

---
+++ test/src/test/Test.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author test
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    public String bonjour(){
+        return "bonjour";
+    }
+    
+}
